69ebf94b7df253228c2b2f5223ad86630c7d94d3
Matt Nerger <matthewjnerger3@gmail.com>
2024-01-22 17:30:21 -0500

    Add comments

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default {
-  vaultNotesPath: '/Users/j/Library/Mobile Documents/com~apple~CloudDocs/Documents/Writing',
+  vaultNotesPath:
+    '/Users/j/Library/Mobile Documents/com~apple~CloudDocs/Documents/Writing',
   dailiesDir: 'dailies/',
   weekliesDir: 'weeklies/',
   sectionsToPull: ['Writing', 'Books'],
--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-
 import './lib/dateExt.js';
-import { getNotes, mergeWeekContent, writeNote } from './lib/obsidian-to-astro.js';
+import { getNotes, mergeWeekContent, writeNote } from 
+	'./lib/obsidian-to-astro.js';
 
 import config from './config.js';
 
-// manually doing our weeks now, so as to make it easier
-const firstDay = new Date( "Jan 8 2024" );
+// manually doing our weeks now, so as to make it easier; set the week
+const firstDay = new Date( "Jan 15 2024" );
 
+// @var reflexLength   Set how mnany days to run the script on. Set to 7 now, 
+//					   for weeklies, but change for months, etc
 const reflexLength = 7;
+
+// Dynamically create an array of dates `reflexLength` long starting from 
+// `firstDay`
 const datesToPull = [ ...Array(reflexLength)].map( (empty, offset) => {
-  const tempDay = new Date( firstDay.toString( ));
-  tempDay.setDate( tempDay.getDate() + offset )
-  return tempDay
+	const tempDay = new Date( firstDay.toString( ));
+	tempDay.setDate( tempDay.getDate() + offset )
+	return tempDay
 });
 
+// Create a list of obsidian file names from the above `datesToPull` array
 const filesToGather = datesToPull.map( date => {
   return config.dailiesDir + date.getYYYYMMDD() + '.md';
 })
 
+// @var allNotes    All the notes we're pulling from, called from external func
 const allNotes = await getNotes(filesToGather);
 
 const weekNoteTitle = config.weekliesDir + firstDay.getYYYYwWW() + '.md';
